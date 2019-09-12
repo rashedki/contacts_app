7bbd65a99a3dedd98419e8cb9a30409744c91124
Khalil <rashedki@hotmail.com>
2019-09-12 08:18:24 +0300

    added satate to app.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- } 
-];
+const contacts =
 
 
 class App extends Component {
-  render() {
+  state = {
+    contacts: [
+     {
+       "id": "karen",
+       "name": "Karen Isgrigg",
+       "handle": "karen_isgrigg",
+       "avatarURL": "http://localhost:5001/karen.jpg"
+     },
+     {
+       "id": "richard",
+       "name": "Richard Kalehoff",
+       "handle": "richardkalehoff",
+       "avatarURL": "http://localhost:5001/richard.jpg"
+     },
+     {
+       "id": "tyler",
+       "name": "Tyler McGinnis",
+       "handle": "tylermcginnis",
+       "avatarURL": "http://localhost:5001/tyler.jpg"
+     },
+    ];
+  }
+  render(){
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
