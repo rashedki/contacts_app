e5508274406c010caca9550dd86781bc849b8f53
Khalil <rashedki@hotmail.com>
2019-09-12 09:37:08 +0300

    installed prop-type and added prop
    added propType to check if the error message appear when we make a mistake or not...

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 	function ListContacts(props){
 	    return (
@@ -25,5 +26,9 @@ import React from 'react'
 	    )
 	  }
 
+		ListContacts.propTypes = {
+			contacts: PropTypes.array.isRequired,
+			onDeleteContact: PropTypes.func.isRequired,
+		}
 
 	export default ListContacts;
